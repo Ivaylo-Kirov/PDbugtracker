568db5aa292643223f1846cc15a7f6c2844838ee
Ivaylo-Kirov <ivo.kirov@gmail.com>
2020-11-19 18:01:56 +0200

    Fixed profile save args kwargs bug

--- bugtracker/users/models.py
+++ bugtracker/users/models.py
@@ -10,9 +10,8 @@ class Profile(models.Model):
     def __str__(self):
         return f'{self.user.username} Profile'
 
-    def save(self): #override the parent save method in order to resize the image
-        # super().save(*args, **kwargs) #don't lose the actual save functionality - added args and kwargs as Corey said this was needed prior to live deployment
-        super().save() # removed them for now or login wasn't working on Staging
+    def save(self, *args, **kwargs): #override the parent save method in order to resize the image
+        super().save(*args, **kwargs) #don't lose the actual save functionality - added args and kwargs as Corey said this was needed prior to live deployment
         img = Image.open(self.image.path) #create an image instance. Image is the Pillow class. "self.image.path" refers to the Profile field image from the model definition
 
         if img.height > 300 or img.width > 300:
