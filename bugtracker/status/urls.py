634307011a41dd5420cae4db4388489b10def5e1
Ivaylo-Kirov <ivo.kirov@gmail.com>
2020-12-19 09:39:01 -0500

    visual tweaks

--- bugtracker/status/urls.py
+++ bugtracker/status/urls.py
@@ -4,6 +4,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="status-home"),
-    path('about/', views.about, name="status-about"),
     path('contactadmin/', views.ContactFormView.as_view(), name="contact-admin")
 ]
--- bugtracker/status/views.py
+++ bugtracker/status/views.py
@@ -11,14 +11,7 @@ items = [
 
 # Create your views here.
 def home(request):
-    context = {
-        'items': items,
-        'title': 'Home'
-    }
-    return render(request, 'status/home.html', context)
-
-def about(request):
-    return render(request, 'status/about.html', {'title': 'About'})
+    return render(request, 'status/home.html')
 
 class ContactFormView(FormView):
     form_class = ContactForm
@@ -26,7 +19,6 @@ class ContactFormView(FormView):
     success_url = '/'
 
     def form_valid(self, form):
-
         # this method gets called when valid data was submitted and it expects an HttpResponse
         form.send_email() # form here represents the ContactForm instance defined in forms.py as a class
         messages.success(self.request, f'Your email has been sent. Thank you.')
